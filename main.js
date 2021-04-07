4ef080596951adc1585deec37208d0e4f01dd79e
Konrad Bujas <kondziobu@gmail.com>
2021-04-07 12:56:56 +0200

    add BEM to other sites, add inner.html method to paragraph

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ const firstName="Konrad";
 const age=31.5;
 
 console.log(`nazywam się ${firstName} i mam ${age} lat(a)`)
+
+
+const heading = document.querySelector('.main__heading--js');
+
+
+heading.innerHTML = `Witaj <span>Drogi</span> Odwiedzający, mam na imię ${firstName} i mam ${age} lat(a)`
+
+const emptyParagraph = document.querySelector('.learn-technique__description--js');
+
+emptyParagraph.innerHTML = `w ćwiczeniu używamy metody inner.HTML i za jej pomocą możemy "wstrzyknąć"
+zawartość do wybranego elemnentu - chyba się udało ;))`
