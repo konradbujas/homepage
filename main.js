6ed568b2076af28dab8f80bddd9ea35c8e7dc8ec
Konrad Bujas <kondziobu@gmail.com>
2021-04-26 08:57:46 +0200

    add greet in console and normalize.css

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 console.log("siema!")
 
 const firstName="Konrad";
-const age=31.5;
+const age=32;
 
 console.log(`nazywam się ${firstName} i mam ${age} lat(a)`)
 
@@ -16,3 +16,44 @@ const emptyParagraph = document.querySelector('.learn-technique__description--js
 
 emptyParagraph.innerHTML = `w ćwiczeniu używamy metody inner.HTML i za jej pomocą możemy "wstrzyknąć"
 zawartość do wybranego elemnentu - chyba się udało ;))`
+
+function calculate(myNumber) {
+    console.log(`Dostałam ${myNumber}`);
+    myNumber = myNumber*7;
+    return myNumber;
+}
+
+const myResult = calculate(age);
+
+console.log(age);
+
+console.log(myResult);
+console.log(age);
+
+
+
+function greet(age, firstName) {
+    console.log(`Witaj Drogi Odwiedzający, mam na imię ${firstName} i mam ${age} lat(a)`);
+
+}
+
+greet(age, firstName);
+
+greet(18, 'Jacek');
+
+function createContent(querySelectorContent, content){
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+}
+
+createContent('.learn-technique__title--js', 'Zmieniony Nagłówek !');
+createContent('.learn-technique__descriptionParagraph--js', 'tutaj zmieniam zawartość paragrafu ;) hehhe...');
+
+
+const greetNew = (age, firstName) => {
+    console.log(
+        `Witaj Drogi Odwiedzający, mam na imię ${firstName} i mam ${age} lat(a)`
+        );
+
+}
+greetNew(3,'Milena');
